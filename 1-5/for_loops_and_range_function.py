e6c8736171ff8c5cebc9dbb5fa41be36771fc8be
KurtReyn <kurtreyn@yahoo.com>
2022-10-27 11:04:24 -0400

    add more lessons for day 5

---
+++ 1-5/for_loops_and_range_function.py
@@ -0,0 +1,18 @@
+# Range
+# for item in range(start, end)
+#     print(item)
+
+# for item in range(start, end, step_by_amount)
+#     print(item)
+
+
+# for number in range(1, 11):
+#     print(number)
+
+# for num in range(1,11, 3):
+#     print(num)
+
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
