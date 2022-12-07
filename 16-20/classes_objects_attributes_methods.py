3e8910452bbb50fe0ce136b2747b14f0d8c559e2
kurtreyn <kurtreyn@yahoo.com>
2022-12-07 15:44:13 -0500

    added new lessons

---
+++ 16-20/classes_objects_attributes_methods.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import prettytable
+# Attributes are variables in a class
+# Methods are functions in a class
+timmy_turtle = Turtle()
+
+# print(timmy_turtle)
+
+timmy_turtle.shape("turtle")
+timmy_turtle.color("coral")
+timmy_turtle.forward(100)
+
+my_screen = Screen()
+# print(my_screen.canvheight)
+
+# my_screen.exitonclick()
