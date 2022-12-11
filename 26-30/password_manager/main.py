0a606ea1a3bf6f1efefaa650e0b1ca2941b9ab94
kurtreyn <kurtreyn@yahoo.com>
2022-12-11 17:16:27 -0500

    continue working on password manager

--- 26-30/password_manager/main.py
+++ 26-30/password_manager/main.py
@@ -6,18 +6,60 @@ import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+	website = website_entry.get()
+	email = email_entry.get()
+	password = password_entry.get()
+
+	if len(website) == 0 or len(password) == 0:
+		messagebox.showinfo(title="Uh Oh!", message="Nothing was entered")
+	else:
+		is_ok = messagebox.askokcancel(title=website, message=f"Details entered: \nEmail: {email} \nPassword: {password} \nOk to save?")
+
+		if is_ok:
+			with open("data.txt", "a") as data_file:
+				data_file.write(f"{website} | {email} | {password}\n")
+				website_entry.delete(0, END)
+				password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50, bg='white')
 
-canvas = Canvas(height=200, width=200)
+canvas = Canvas(height=200, width=200, bg='white')
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website", bg='white', fg='black')
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username", bg='white', fg='black')
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password", bg='white', fg='black')
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35, bg='white', fg='black', insertbackground='black', highlightthickness=0)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35, bg='white', fg='black', insertbackground='black', highlightthickness=0)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "kurtreyn@yahoo.com")
+password_entry = Entry(width=21, bg='white', fg='black', insertbackground='black', highlightthickness=0)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.config(borderwidth=0)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36,command=save)
+add_button.config(borderwidth=1, highlightthickness=1)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
 
