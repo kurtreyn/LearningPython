45c4842c451d66a65c29c78425e386f941e2ccbe
kurtreyn <kurtreyn@yahoo.com>
2022-12-13 00:26:10 -0500

    finish tkinter lesson

--- 26-30/tkinter_lessons/tkinter_lessons.py
+++ 26-30/tkinter_lessons/tkinter_lessons.py
@@ -1,12 +1,40 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Tkinter GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=2, pady=2)
 
-# Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+
+
+def button_clicked():
+	print("I got clicked")
+	input_text = input_element.get()
+	label.config(text=input_text)
+
+# Labels
+label = Label(text="I am a label", font=("Arial", 24, "bold"))
+#label.pack(expand=True)
+label.grid(column=0, row=0)
+
+
+# my_label["text"] = "New Text"
+# my_label.config(text="New text via config")
+
+
+# Buttons
+button = Button(text="Click Me", command=button_clicked)
+# button.pack()
+# button.place(x=0, y=100)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+
+# Entries
+input_element = Entry(width=10)
+input_element.grid(column=3, row=3)
 
 
 window.mainloop()
